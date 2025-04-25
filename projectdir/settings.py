c2d89fcac15f69743717a9013c329a8ac1f2df00
anandj1 <anandj12215@gmail.com>
2025-04-25 11:05:20 +0530

    added static files

--- projectdir/settings.py
+++ projectdir/settings.py
@@ -39,6 +39,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     # Ensure that your custom middleware does not block access
     # Uncomment the next line if CheckUser middleware is needed
     # 'projectdir.middleware.CheckUser',  
